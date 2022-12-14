3d0fb40a6555bc929e3d3138665649df8e3820de
sarahth25 <64575656+sarahth25@users.noreply.github.com>
2022-12-15 00:41:50 +0300

    reemlastnight

---
+++ Design-main/Assets/Scripts/backto6x4x6.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class backto6x4x6 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void StartButton(){
+        SceneManager.LoadScene("6x4x6");
+            }
+
+    // Update is called once per frame
+    
+}
---
+++ Design-main/Assets/Scripts/reterncan.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class reterncan : MonoBehaviour
+{
+    public GameObject canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        
+    }
+
+    public void create_furn()
+    {
+        
+        canvas.SetActive(true);
+    }
+}
